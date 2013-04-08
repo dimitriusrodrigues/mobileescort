b7803c3580be774b450bd2baa9052f8d8efad733
Dimitrius Rodrigues <dimitrius.menezes@gmail.com>
2013-04-08 09:48:44 -0300

    Teste de commit git.

--- src/com/mobileescort/mobileescort/Cadastro.java
+++ src/com/mobileescort/mobileescort/Cadastro.java
@@ -13,6 +13,7 @@ public class Cadastro extends Activity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_cadastro);
         
     }
