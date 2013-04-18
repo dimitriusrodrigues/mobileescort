477237a41519b91ae3c502d95424eebb63aab956
dimitrius.menezes <dimitrius.menezes@gmail.com>
2013-04-18 19:38:23 -0300

    Alteração de botões na tela de cadastro.

--- src/com/mobileescort/mobileescort/Cadastro.java
+++ src/com/mobileescort/mobileescort/Cadastro.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 public class Cadastro extends Activity {
 	
 	//teste
+	// 18042013 DimitriusRodrigues : Alterado nome dos botões.
 	
 	Button btEnviar;
 	EditText etNome;
