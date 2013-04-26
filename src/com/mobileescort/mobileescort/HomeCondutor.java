ea392c43d4be485ab0d6d65e01fdbe9d390bccf9
Willian <williantufao@hotmail.com>
2013-04-25 22:26:40 -0300

    Alteração na classe HomeCondutor.

--- src/com/mobileescort/mobileescort/HomeCondutor.java
+++ src/com/mobileescort/mobileescort/HomeCondutor.java
@@ -1,11 +1,8 @@
 package com.mobileescort.mobileescort;
 
-import com.mobileescort.mobileescort.utils.RegisterActivity;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
