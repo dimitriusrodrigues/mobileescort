656f1005934b55593ec40ad8a97146434c5c46e8
Willian <williantufao@hotmail.com>
2013-04-25 22:26:22 -0300

    Alteração na classe Home.

--- src/com/mobileescort/mobileescort/Home.java
+++ src/com/mobileescort/mobileescort/Home.java
@@ -3,10 +3,8 @@ package com.mobileescort.mobileescort;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Home extends Activity {
