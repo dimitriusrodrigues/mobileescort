26b66b3d3880cede40ead037ffbd4b15e38f707a
Willian <williantufao@hotmail.com>
2013-04-25 22:29:54 -0300

    Alteração nas notificações.

--- src/com/mobileescort/mobileescort/Notificar.java
+++ src/com/mobileescort/mobileescort/Notificar.java
@@ -2,8 +2,6 @@ package com.mobileescort.mobileescort;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
